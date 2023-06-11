d779241c667df1b26fe6b3043251c9d7bd10c3d4
Akhil-Sharma-26 <asharma1_be22@thapar.edu>
2023-06-11 18:12:02 +0530

    feed-completed

--- client/src/Components/VideoCard.jsx
+++ client/src/Components/VideoCard.jsx
@@ -4,13 +4,20 @@ import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../Assets/constants";
-
-const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
+// Below here, we are destructuring the id and then again videoID and then we are destructuring the snippet
+const VideoCard = ({ video: { id: { videoId }, snippet } }) => ( // Card media contains image of thumbnail,
+// Link component must have a To property
+// here, To: If videoId exists(?) then go to the video/id, if not exist then go to demovidURL
+// in the alt property then it shows the title a/c too the snippet.
+// ? Learn how to set the size acordingly from any other source...
+// Cardcontent is actually the Card that is holding the photo, title, channel etc. and inside it we are using typography to type our Title upto 60 characters using slice property || if the title doesn't exit then render DemoTitle
+// Now in the cardContent we are poiinting to the channelID instead of videoID. using the same logic as CardMedia. CheckCircle is nothing but the veriffied
+// The outterMost Card, we are fixing the size of the Card so that it doesn't expands much
   <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
     <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
       <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
         sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }} 
-      />
+      /> 
     </Link>
     <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
